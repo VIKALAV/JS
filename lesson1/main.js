a35b04bbc43874107ea3a2d9a7929a0df5f2dfbb
VIKALAV <lavrenko_vika@ukr.net>
2022-07-24 19:08:00 +0300

    Initial commit

---
+++ lesson1/main.js
@@ -0,0 +1,106 @@
+let str1 = 'hello ';
+console.log(str1);
+alert(str1);
+document.write (str1);
+
+let str2 = 'owu ';
+console.log(str2);
+alert(str2);
+document.write (str2);
+
+let str3 = 'com ';
+console.log(str3);
+alert(str3);
+document.write(str3);
+
+let str4 = 'ua ';
+console.log(str4);
+alert(str4);
+document.write(str4);
+
+let n1=1 ;
+console.log(n1);
+alert(n1);
+document.write(n1+''+' ');
+
+let n2 = 10;
+console.log(n2);
+alert(n2);
+document.write(n2+''+' ');
+
+let n3 = 1;
+let n4=1000;
+console.log(n3-n4);
+alert(n3-n4);
+document.write((n3-n4)+''+' ');
+
+let n5 = 123;
+console.log(n5);
+alert(n5);
+document.write(n5+''+' ');
+
+let n6 = 3.14;
+console.log(n6);
+alert(n6);
+document.write(n6+''+ ' ');
+
+let n7 = 3;
+let n8 =0.9;
+console.log(n7*n8);
+alert(n7*n8);
+document.write((n7*n8)+''+' ');
+
+let n9 = 16;
+console.log(n9);
+alert(n9);
+document.write(n9+''+' ');
+
+let n10 = 5;
+let n11= 10;
+console.log(n10<n11);
+alert(n10<n11);
+document.write((n10<n11)+''+' ');
+
+let n12 = 5;
+let n13 = 10;
+console.log(n12>n13);
+alert(n12>n13);
+document.write((n12>n13)+''+' ');
+
+let str5 ='Viktoriia';
+let str6='Mykhaylivna';
+let str7='Lavrenko';
+let words = `${str5} ${str6} ${str7};`
+console.log(words);
+
+let a=100;
+console.log(typeof a);
+let b='100';
+console.log(typeof b);
+let c=true;
+console.log(typeof c);
+
+
+let name=prompt('What is your name?', 'Viktoriia');
+let middlename=prompt( "What is your middlename?",'Mykhaylivna');
+let lastname=prompt('What is your lastname?', 'Lavrenko');
+let age=+prompt('How old are you? ', '37');
+document.write(name+' '+middlename+' '+lastname+' '+age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
